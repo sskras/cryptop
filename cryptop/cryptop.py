b5519a0fa047f6c54e4a1e2c4742332e1e25b111
Patrick Doetsch <pdoetsch@apptek.com>
2020-05-24 21:49:07 +0200

    cryptocompare

--- cryptop/cryptop.py
+++ cryptop/cryptop.py
@@ -100,25 +100,30 @@ def read_configuration(confpath):
   CONFIG.read(confpath)
   return CONFIG
 
+CONFIG = read_configuration(CONFFILE)
 rget = lambda url:requests.get(url,timeout=7).json()
 coinstats = {}
-coinmap = {'KNC' : 'kyber-network', 'BTG' : 'bitcoin-gold'}
+
+clist = []
 try:
-  CCLIST = rget('https://min-api.cryptocompare.com/data/all/coinlist/')['Data']
+  CCLIST = rget('https://min-api.cryptocompare.com/data/blockchain/list?api_key='+CONFIG['keys'].get('cryptocompare', ''))['Data']
 except:
-  CCLIST = []
+  CCLIST = {}
 CGMAP = {x['symbol'].upper() : x['id'] for x in rget('https://api.coingecko.com/api/v3/coins/list')}
+CCLIST = set(CCLIST.keys())
 CCSET = set([])
+
 def update_coins():
   global CURRENCYLIST
+  wallet = read_wallet()
+  #CCSET = set(wallet.keys()) - CCLIST
   stats = {}
   cmclist = set([])
   for coin in [ c for c in CURRENCYLIST if not isfiat(c) ]:
     try:
-      ret = rget('https://min-api.cryptocompare.com/data/histohour?fsym=%s&tsym=USD&toTs=%d&limit=175' % (coin.upper(),int(time.time())))
-      cmclist.add(coin.upper())
+      ret = rget('https://min-api.cryptocompare.com/data/histohour?fsym=%s&tsym=USD&toTs=%d&limit=175&api_key=%s' %
+        (coin.upper(),int(time.time()),CONFIG['keys'].get('cryptocompare', '')))
     except:
-      cmclist.add(coin.upper())
       continue
     if ret['Data']:
       if not coin in stats.keys():
@@ -127,8 +132,6 @@ def update_coins():
       stats[coin]['percent_change_1h'] = 100. - 100. * (ret['Data'][-2]['close'] / ret['Data'][-1]['close'])
       stats[coin]['percent_change_24h'] = 100. - 100. * (ret['Data'][-25]['close'] / ret['Data'][-1]['close'])
       stats[coin]['percent_change_7d'] = 100. - 100. * (ret['Data'][-169]['close'] / ret['Data'][-1]['close'])
-    else:
-      cmclist.add(coin.upper())
 
   cmc = http.client.HTTPSConnection("api.coinmarketcap.com")
   clist = []
@@ -168,10 +171,42 @@ def update_coins():
       stats[fiat]['percent_change_1h'] = stats[fiat]['percent_change_24h'] / 24.
       stats[fiat]['percent_change_7d'] = 100. - 100. * rates[7][1] / rates[0][1]
 
+  for tok in CCSET:
+    if tok.upper() in CGMAP:
+      try:
+        ret = rget('https://api.coingecko.com/api/v3/coins/' + CGMAP[tok.upper()])
+        if ret['market_data']['total_volume']['usd'] == 0:
+          continue
+        if not tok in stats:
+          stats[tok] = {}
+          for d in ['1h','24h', '7d']:
+            stats[tok]['percent_change_' + d] = 0
+        stats[tok]['price_usd'] = ret['market_data']['current_price']['usd']
+        stats[tok]['24h_volume_usd'] = ret['market_data']['total_volume']['usd']
+        if stats[tok]['24h_volume_usd'] == 0:
+          for alt in ['eur','btc','eth']:
+            if alt in ret['market_data']['total_volume']:
+              stats[tok]['24h_volume_usd'] = ret['market_data']['total_volume'][alt] * stats[alt.upper()]['price_usd']
+              break
+        for d in ['1h','24h', '7d']:
+          if 'usd' in ret['market_data']['price_change_percentage_%s_in_currency'%d]:
+            stats[tok]['percent_change_' + d] = ret['market_data']['price_change_percentage_%s_in_currency'%d]['usd']
+          else:
+            for alt in ['eur','btc','eth']:
+              if alt in ret['market_data']['price_change_percentage_%s_in_currency'%d]:
+                stats[tok]['percent_change_' + d] = ret['market_data']['price_change_percentage_%s_in_currency'%d][alt] * stats[alt.upper()]['price_usd']
+                break
+      except:
+        continue
+
   if CCSET:
     try:
-      ret = rget('https://min-api.cryptocompare.com/data/pricemultifull?fsyms=%s&tsyms=USD' % ','.join(list(CCSET)))
-      for tok in ret['RAW']:
+      ret = rget('https://min-api.cryptocompare.com/data/pricemultifull?fsyms=%s&tsyms=USD&api_key=%s' %
+        (','.join(list(CCSET)), CONFIG['keys'].get('cryptocompare', '')))
+    except:
+      pass
+    for tok in ret['RAW']:
+      if tok in stats.keys() and not isfiat(tok):
         rates = [ stats[tok]['price_usd'],
         stats[tok]['price_usd'] * (1. - stats[tok]['percent_change_1h'] / 100.),
         stats[tok]['price_usd'] * (1. - stats[tok]['percent_change_24h'] / 100.),
@@ -184,8 +219,13 @@ def update_coins():
           stats[tok]['percent_change_1h'] = 100. - 100. * rates[1] / rates[0]
           stats[tok]['percent_change_24h'] = 100. - 100. * rates[2] / rates[0]
           stats[tok]['percent_change_7d'] = 100. - 100. * rates[3] / rates[0]
-    except:
-      pass
+      else:
+        stats[tok] = {}
+        stats[tok]['price_usd'] = ret['RAW'][tok]['USD']['PRICE']
+        stats[tok]['percent_change_1h'] = ret['RAW'][tok]['USD']['CHANGEPCTHOUR']
+        stats[tok]['percent_change_24h'] = ret['RAW'][tok]['USD']['CHANGEPCT24HOUR']
+        stats[tok]['percent_change_7d'] = 0
+        stats[tok]['24h_volume_usd'] = ret['RAW'][tok]['USD']['TOTALVOLUME24HTO']
 
   try:
     ret = rget('https://bittrex.com/api/v1.1/public/getmarketsummaries')
@@ -202,11 +242,14 @@ def update_coins():
           stats[tok]['price_usd'] * (1. - stats[tok]['percent_change_7d'] / 100.) ]
           price = stats[tok]['price_usd'] / stats['ETH']['price_usd']
           prev = stats[tok]['price_usd']
+          if prev == 0:
+            prev = price if price > 0 else 1
           stats[tok]['price_usd'] = (0.75 * float(pair['Last']) + 0.25 * price) * stats['ETH']['price_usd']
           rates = [ r + (stats[tok]['price_usd'] - prev) * r / prev for r in rates ]
           stats[tok]['percent_change_1h'] = 100. - 100. * rates[1] / rates[0]
           stats[tok]['percent_change_24h'] = 100. - 100. * rates[2] / rates[0]
           stats[tok]['percent_change_7d'] = 100. - 100. * rates[3] / rates[0]
+
   try:
     ret = rget('https://www.binance.com/api/v1/ticker/allPrices')
   except:
@@ -239,34 +282,6 @@ def update_coins():
             stats[tok]['percent_change_24h'] = 0
             stats[tok]['percent_change_7d'] = 0
             stats[tok]['24h_volume_usd'] = 0
-
-  for tok in CCSET:
-    if tok.upper() in CGMAP:
-      try:
-        ret = rget('https://api.coingecko.com/api/v3/coins/' + CGMAP[tok.upper()])
-        if ret['market_data']['total_volume']['usd'] == 0:
-          continue
-        if not tok in stats:
-          stats[tok] = {}
-          for d in ['1h','24h', '7d']:
-            stats[tok]['percent_change_' + d] = 0
-        stats[tok]['price_usd'] = ret['market_data']['current_price']['usd']
-        stats[tok]['24h_volume_usd'] = ret['market_data']['total_volume']['usd']
-        if stats[tok]['24h_volume_usd'] == 0:
-          for alt in ['eur','btc','eth']:
-            if alt in ret['market_data']['total_volume']:
-              stats[tok]['24h_volume_usd'] = ret['market_data']['total_volume'][alt] * stats[alt.upper()]['price_usd']
-              break
-        for d in ['1h','24h', '7d']:
-          if 'usd' in ret['market_data']['price_change_percentage_%s_in_currency'%d]:
-            stats[tok]['percent_change_' + d] = ret['market_data']['price_change_percentage_%s_in_currency'%d]['usd']
-          else:
-            for alt in ['eur','btc','eth']:
-              if alt in ret['market_data']['price_change_percentage_%s_in_currency'%d]:
-                stats[tok]['percent_change_' + d] = ret['market_data']['price_change_percentage_%s_in_currency'%d][alt] * stats[alt.upper()]['price_usd']
-                break
-      except:
-        continue
 
   for tok in stats:
     for k in ['24h_volume_usd']:
@@ -851,9 +866,6 @@ def main():
   if os.path.isfile(BASEDIR):
     sys.exit('Please remove your old configuration file at {}'.format(BASEDIR))
   os.makedirs(BASEDIR, exist_ok=True)
-
-  global CONFIG
-  CONFIG = read_configuration(CONFFILE)
 
   global BLACKLIST, CURRENCYLIST, CURRENCY, SYMBOL, SYMBOLMAP
   BLACKLIST = CONFIG['api'].get('blacklist', '').split(',')
